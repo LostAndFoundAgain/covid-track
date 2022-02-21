f86afad1757c1a24036f2f3a7fef59593d63d1ec
Nikhil <nikrkt1@gmail.com>
2022-02-21 19:07:54 +0530

    Initial commit

---
+++ src/main/java/io/winter/covidtrack/CovidTrackApplication.java
@@ -0,0 +1,13 @@
+package io.winter.covidtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidTrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovidTrackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/winter/covidtrack/CovidTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.winter.covidtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidTrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
